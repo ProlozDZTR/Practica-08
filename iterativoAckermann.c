f77bce32e414d03f470a48a32bf54c0a7c5a80a4
tlato423 <yomeroxd3@gmail.com>
2025-11-13 23:16:31 -0600

    iterativo Levi ackerman y recursivo de particion

--- iterativoAckermann.c
+++ iterativoAckermann.c
@@ -2,23 +2,59 @@
 
 #include <stdio.h>
 
+#define MAX_PILA 100
+
 void ingresoNumeros(int *x,int *y){
     printf("Digite dos numeros: ");
-    scanf("%i %i",x,y);
+    scanf("%d %d",x,y);
 }
 
-int ackermann(int x,int y){
-    int aux;
+int ackermann(int m,int n){
+int stack[MAX_PILA];
+    int top = 0; 
+    long long n_actual = n; 
+    int m_actual = m;
+
+    if (m < 0 || n < 0) {
+        printf("Error: Los numeros deben ser no negativos.\n");
+        return -1;
+    }
+
+    stack[top++] = m_actual; 
+
+    while (top > 0) {
+        m_actual = stack[top - 1]; 
+        
+        if (m_actual == 0) {
+            n_actual = n_actual + 1;
+            top--; 
+            
+        } else if (n_actual == 0) {
+            stack[top - 1] = m_actual - 1; 
+            n_actual = 1; 
+            
+        } else {
+            n_actual = n_actual - 1; 
+
+            if (top >= MAX_PILA) {
+                printf("\nERROR: Desbordamiento de pila. Los numeros son muy grandes.\n");
+                return -1; 
+            }
+            stack[top++] = m_actual - 1;
+        }
+    }
+    
+    return n_actual;
 }
 
 void ejecutarPrograma(){
-    int resultado;
+    long long resultado;
     int numUno = 0,numDos = 0;
 
-    ingresosNumeros(&numUno,&numDos);
+    ingresoNumeros(&numUno,&numDos);
     resultado = ackermann(numUno,numDos);
 
-    printf("El ackermann de %d y %d es: %d",numUno,numDos,resultado);
+    printf("El ackermann de %d y %d es: %lld",numUno,numDos,resultado);
 }
 
 int main(int argc, char const *argv[]) {
---
+++ recursividadParticion.c
@@ -0,0 +1,35 @@
+/*Problema 5: funcion recursiva de particion*/
+
+#include <stdio.h>
+
+void ingresoNumeros(int *x,int *y){
+    printf("Digite dos numeros");
+    scanf("%d %d",x,y);
+}
+
+int particion(int m, int n){
+    if(m == 1 || n == 1){
+        return 1;
+    }else if(m < n){
+        return particion(m, m);
+    }else if(m == n){
+        return 1 + particion(m, m - 1 );
+    }else if(m > n){
+        return particion(m,n-1) + particion(m-n,n);
+    }
+}
+
+void ejecutarPrograma(){
+    int resultado; 
+    int numUno = 0,numDos = 0;
+
+    ingresoNumeros(&numUno,&numDos);
+    resultado = particion(numUno,numDos);
+
+    printf("la particion de %d y %d es: %d",numUno,numDos,resultado);
+}
+
+int main(int argc, char const *argv[]){
+    ejecutarPrograma();
+    return 0;
+}
